3ac5e2dabb89eedba7bed5d581e965addfb6a864
Hugo <hugojavidferrero@gmail.com>
2025-01-28 08:27:26 +0100

    corrigiendo errores

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module org.hugo.dein.jasperejercicio3 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires jasperreports;
+    requires net.sf.jasperreports.core;
 
 
     opens org.hugo.dein.jasperejercicio3 to javafx.fxml;
